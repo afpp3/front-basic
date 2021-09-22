419eff9058102933d26d12a30776f21255f4cd37
Andre <afelipp3@gmail.com>
2021-09-22 18:15:30 -0300

    🎉 First Commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.querySelector("[data-js='app']").innerHTML = `
+<h1>Hello Vite!</h1>
+<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
